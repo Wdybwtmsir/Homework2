f4262c527e9fedce8a42e97d2c414c731c63f069
khrom <khrom@DESKTOP-DA2UCIR>
2024-10-10 13:33:10 +0200

    Добавьте файлы проекта.

---
+++ Homework2/Program.cs
@@ -0,0 +1,17 @@
+﻿try
+{
+    Console.WriteLine("Введите число a");
+    Double a = Double.Parse(Console.ReadLine());
+    Console.WriteLine("Введите число b");
+    Double b = Double.Parse(Console.ReadLine());
+    Console.WriteLine("Введите число c");
+    Double c = Double.Parse(Console.ReadLine());
+    if (a * a + b * b == c * c) 
+         Console.WriteLine("Треугольник прямоугольный");
+    else
+         Console.WriteLine("Треугольник не прямоугольный");
+}
+catch
+{
+    Console.WriteLine("Введите правильные значения");
+}
